34cb7cfcbb0def8064233aa0debf95a54b19532e
modeeee <sagievartur@mail.ru>
2026-05-25 16:20:35 +0500

    feat: add migrations

---
+++ migrations/env.py
@@ -0,0 +1,62 @@
+from logging.config import fileConfig
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import pool
+from alembic import context
+
+config = context.config
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+from app.core.database import Base 
+from app.models import User
+from app.core.config import settings  
+
+target_metadata = Base.metadata
+
+
+def get_url():
+    return settings.DB_URL
+
+
+async def run_migrations_online():
+    """Run migrations in 'online' mode."""
+    connectable = create_async_engine(
+        get_url(),
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+def do_run_migrations(connection):
+    """Эта функция вызывается внутри run_sync и должна принимать ровно один аргумент"""
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        compare_type=True,
+        compare_server_default=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+if context.is_offline_mode():
+    from sqlalchemy import engine_from_config, pool
+
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+        with context.begin_transaction():
+            context.run_migrations()
+else:
+    import asyncio
+    asyncio.run(run_migrations_online())
